8293c0a00a7d6f507cd2286274920f07ce38dd2f
TaeyeongHan <dianspace53@gmail.com>
2025-03-13 00:16:30 +0900

    시작 메뉴 UI 초기 설정 단계입니다.

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void start()
     {
         Debug.Log("test");
+       
         Debug.Log("test2");
-    }
+   }
 
     // Update is called once per frame
     void Update()
