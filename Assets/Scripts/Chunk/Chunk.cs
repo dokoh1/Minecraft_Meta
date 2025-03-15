fd8e9eae9c53a9a737a69f1ce823fc643e911b92
dokoh1 <146950802+dokoh1@users.noreply.github.com>
2025-03-15 12:25:59 +0900

    청크 생성 및 atlas 텍스처 적용#11

--- Assets/Scripts/Chunk/Chunk.cs
+++ Assets/Scripts/Chunk/Chunk.cs
@@ -1,107 +1,147 @@
-
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
-    private MeshRenderer renderer;
-    private MeshFilter meshFilter;
+    public BlockData blockData;
+
+    private MeshRenderer _renderer;
+    private MeshFilter _meshFilter;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    int vertexIndex = 0;
+    private int _vertexIndex = 0;
+    private readonly BlockTypeEnum[,,] _blockNames = new BlockTypeEnum[16, 256, 16];
     
-    List<Vector3> vertices = new List<Vector3>();
-    List<int> indices = new List<int>();
-    List<Vector2> uvs = new List<Vector2>();
-    
-    bool [,,] voxelCheck = new bool[VoxelData.chunkWidth, VoxelData.chunkHeight, VoxelData.chunkDepth];
-    void Start()
+    private readonly List<Vector3> _vertices = new();
+    private readonly List<int> _indices = new();
+    private readonly List<Vector2> _uvs = new();
+
+
+    private void Awake()
     {
-        renderer = GetComponent<MeshRenderer>();
-        meshFilter = GetComponent<MeshFilter>();
-        
-        InitVoxelCheck();
+        blockData = GameObject.Find("BlockData").GetComponent<BlockData>();
+        _renderer = GetComponent<MeshRenderer>();
+        _meshFilter = GetComponent<MeshFilter>();
+
+        ChunkTypeSetting();
         CreateVoxelChunk();
         CreateMesh();
     }
-
-    void InitVoxelCheck()
+    
+    void ChunkTypeSetting()
     {
 
-            for (int y = 0; y < VoxelData.chunkHeight; y++)
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
             {
-                for (int x = 0; x < VoxelData.chunkWidth; x++)
+                for (int z = 0; z < VoxelData.ChunkDepth; z++)
                 {
-                    for (int z = 0; z < VoxelData.chunkDepth; z++)
-                    {
-                        voxelCheck[x, y, z] = true;
-                    }
+                    // if (y < 200)
+                    // {
+                    //     blockNames[y, x, z] = "Dirt";
+                    // }
+                    // else
+                    _blockNames[x, y, z] = BlockTypeEnum.Stone;
+
                 }
             }
+        }
     }
 
+    /// <summary>
+    /// 정육면체의 면이 내부에 있는 면인지 외부에 있는 면인지 확인하는 함수
+    /// </summary>
+    /// <param name="pos">위치</param>
+    /// <returns></returns>
     bool IsCheckVoxel(Vector3 pos)
     {
         int x = Mathf.FloorToInt(pos.x);
         int y = Mathf.FloorToInt(pos.y);
         int z = Mathf.FloorToInt(pos.z);
 
-        if (x < 0 || x > VoxelData.chunkWidth - 1 || y < 0 || y > VoxelData.chunkHeight - 1 || z < 0 ||
-            z > VoxelData.chunkDepth - 1)
+        if (x < 0 || x > VoxelData.ChunkWidth - 1 ||
+            y < 0 || y > VoxelData.ChunkHeight - 1 ||
+            z < 0 || z > VoxelData.ChunkDepth - 1)
             return false;
-        return voxelCheck[x, y, z];
+        
+        return blockData.BlockTypeDictionary[_blockNames[x, y, z]].isSolid;
     }
+
     void CreateVoxelChunk()
     {
-        for (int y = 0; y < VoxelData.chunkHeight; y++)
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
         {
-            for (int x = 0; x < VoxelData.chunkWidth; x++)
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
             {
-                for (int z = 0; z < VoxelData.chunkDepth; z++)
+                for (int z = 0; z < VoxelData.ChunkDepth; z++)
                 {
                     AddVoxelChunk(new Vector3(x, y, z));
                 }
             }
         }
     }
+    /// <summary>
+    /// Voxel의 Vertex 정보 및 index정보 및 uv 정보를 넣는다.
+    /// </summary>
+    /// <param name="pos"></param>
     void AddVoxelChunk(Vector3 pos)
     {
         for (int i = 0; i < 6; i++)
         {
-            if (!IsCheckVoxel(pos + VoxelData.faceChecks[i]))
+            if (!IsCheckVoxel(pos + VoxelData.FaceChecks[i]))
             {
-                    // 처음에 vertex 정보를 Index정보를 바탕으로 먼저 그리고 vertexIndex를 넣는다.
-                    vertices.Add(pos + VoxelData.VoxelVertes[VoxelData.VoxelIndex[i, 0]]);
-                    vertices.Add(pos + VoxelData.VoxelVertes[VoxelData.VoxelIndex[i, 1]]);
-                    vertices.Add(pos + VoxelData.VoxelVertes[VoxelData.VoxelIndex[i, 2]]);
-                    vertices.Add(pos + VoxelData.VoxelVertes[VoxelData.VoxelIndex[i, 3]]);
-                    uvs.Add(VoxelData.VoxelUv[0]);
-                    uvs.Add(VoxelData.VoxelUv[1]);
-                    uvs.Add(VoxelData.VoxelUv[2]);
-                    uvs.Add(VoxelData.VoxelUv[3]);
-                    indices.Add(vertexIndex);
-                    indices.Add(vertexIndex + 1);
-                    indices.Add(vertexIndex + 2);
-                    indices.Add(vertexIndex + 2);
-                    indices.Add(vertexIndex + 1);
-                    indices.Add(vertexIndex + 3);
-                    vertexIndex += 4;
+                BlockTypeEnum blockKey = _blockNames[(int)pos.x, (int)pos.y, (int)pos.z];
+                
+                // 처음에 vertex 정보를 Index정보를 바탕으로 먼저 그리고 vertexIndex를 넣는다.
+                _vertices.Add(pos + VoxelData.VoxelVertes[VoxelData.VoxelIndex[i, 0]]);
+                _vertices.Add(pos + VoxelData.VoxelVertes[VoxelData.VoxelIndex[i, 1]]);
+                _vertices.Add(pos + VoxelData.VoxelVertes[VoxelData.VoxelIndex[i, 2]]);
+                _vertices.Add(pos + VoxelData.VoxelVertes[VoxelData.VoxelIndex[i, 3]]);
+
+                AddTexture(blockData.BlockTypeDictionary[blockKey].GetTextureID(i));
+                
+                _indices.Add(_vertexIndex);
+                _indices.Add(_vertexIndex + 1);
+                _indices.Add(_vertexIndex + 2);
+                _indices.Add(_vertexIndex + 2);
+                _indices.Add(_vertexIndex + 1);
+                _indices.Add(_vertexIndex + 3);
+                _vertexIndex += 4;
             }
         }
-        
     }
-
+    /// <summary>
+    /// Vertex, uv, Index 정보를 Mesh에 넣는다.
+    /// </summary>
     void CreateMesh()
     {
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = indices.ToArray();
-        mesh.uv = uvs.ToArray();
+        Mesh mesh = new()
+        {
+            vertices = _vertices.ToArray(),
+            triangles = _indices.ToArray(),
+            uv = _uvs.ToArray()
+        };
         mesh.RecalculateNormals();
-        meshFilter.mesh = mesh;
+        _meshFilter.mesh = mesh;
     }
-    void Update()
+
+    /// <summary>
+    /// Atlas 이미지를 uv 좌표에 맞게 매핑하는 로직을 담은 함수
+    /// </summary>
+    /// <param name="textureID"></param>
+    void AddTexture(int textureID)
     {
-        
+        // ReSharper disable once PossibleLossOfFraction
+        float y = textureID / VoxelData.TextureAtlasSize;
+        float x = textureID - (y * VoxelData.TextureAtlasSize);
+
+        x *= VoxelData.NormalizedBlockTextureSize;
+        y *= VoxelData.NormalizedBlockTextureSize;
+        y = 1f - y - VoxelData.NormalizedBlockTextureSize;
+
+        _uvs.Add(new Vector2(x, y));
+        _uvs.Add(new Vector2(x, y + VoxelData.NormalizedBlockTextureSize));
+        _uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y));
+        _uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y + VoxelData.NormalizedBlockTextureSize));
     }
 }
--- Assets/Scripts/Chunk/VoxelData.cs
+++ Assets/Scripts/Chunk/VoxelData.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 // Data Only Read and Value Not Changed
 public static class VoxelData
 {
-    public static readonly int chunkWidth = 16;
-    public static readonly int chunkDepth = 16;
-    public static readonly int chunkHeight = 16;
+    public static readonly int ChunkWidth = 16;
+    public static readonly int ChunkDepth = 16;
+    public static readonly int ChunkHeight = 30;
+
+    public static readonly int TextureAtlasSize = 16;
+
+    public static float NormalizedBlockTextureSize
+    {
+        get {return 1f / TextureAtlasSize; }
+    }
     
-    //Voxel Vertex
     public static readonly Vector3[] VoxelVertes = new Vector3[]
     {
         new Vector3(0.0f, 0.0f, 0.0f),
@@ -18,8 +24,8 @@ public static class VoxelData
         new Vector3(1.0f, 1.0f, 1.0f),
         new Vector3(0.0f, 1.0f, 1.0f),
     };
-    //Voxel Index
-    public static readonly Vector3[] faceChecks = new Vector3[6]
+
+    public static readonly Vector3[] FaceChecks = new Vector3[6]
     {
         new Vector3(0.0f, 0.0f, -1.0f),
         new Vector3(0.0f, 0.0f, 1.0f),
@@ -28,22 +34,6 @@ public static class VoxelData
         new Vector3(-1.0f, 0.0f, 0.0f),
         new Vector3(1.0f, 0.0f, 0.0f),
     };
-    
-    // public static readonly int[,] VoxelIndex = new int[6, 6]
-    // {
-    //     // 윗면
-    //     {3, 7, 2, 2, 7, 6},
-    //     // 뒷면
-    //     {5, 6, 4, 4, 6, 7},
-    //     //왼쪽면
-    //     {4, 7, 0, 0, 7, 3},
-    //     // 오른쪽면
-    //     {1, 2, 5, 5, 2, 6},
-    //     // 앞면
-    //     {0, 3, 1, 1, 3, 2},
-    //     // 아랫면
-    //     {0, 1, 4, 4, 1, 5}
-    // };
 
     public static readonly int[,] VoxelIndex = new int[6, 4]
     {
@@ -68,4 +58,3 @@ public static class VoxelData
         new Vector2(1.0f, 1.0f),
     };
 }
-
