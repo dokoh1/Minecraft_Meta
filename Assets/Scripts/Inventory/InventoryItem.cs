3c3af42d734bdcea45baf93ba6672764e4af485b
한태영 <dianspace53@gmail.com>
2025-03-16 18:50:42 +0900

    인벤토리 다른 컴에서 이어서 제작함.

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+//이 스크립트는 아이템 개별관리, 드래그 및 드롭을 지원한다.
+//인벤토리 슬롯 안에 들어갈 아이템 개체를 나타내며, 드래그와 드롭이 가능한 구조이다.
+//주요역할: 아이템 정보를 담고 있고, 
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [Header("UI")]
