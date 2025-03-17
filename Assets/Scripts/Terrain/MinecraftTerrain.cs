a29d2a20f080b68886eba478997d70a4ffaf10ac
dokoh1 <146950802+dokoh1@users.noreply.github.com>
2025-03-17 21:01:02 +0900

    Terrain 동적 생성 전 정적 생성

--- Assets/Scripts/Terrain/MinecraftTerrain.cs
+++ Assets/Scripts/Terrain/MinecraftTerrain.cs
@@ -6,26 +6,26 @@ public class MinecraftTerrain : MonoBehaviour
 {
     // private Vector3 SpawnPosition;
     public BlockData blockData;
-    public Transform player;
+    // public Transform player;
     
     private Chunk[,] _chunks = new Chunk[VoxelData.TerrainSize, VoxelData.TerrainSize];
-    private Vector3 _spawnPosition;
+    // private Vector3 _spawnPosition;
     
     private void Awake()
     {
-        _spawnPosition = new Vector3
-            ((VoxelData.TerrainSize * VoxelData.ChunkWidth) / 2, 
-            VoxelData.ChunkHeight + 2,
-            (VoxelData.TerrainSize * VoxelData.ChunkDepth) / 2);
-        player.transform.position = _spawnPosition;
+        // _spawnPosition = new Vector3
+        //     ((VoxelData.TerrainSize * VoxelData.ChunkWidth) / 2, 
+        //     VoxelData.ChunkHeight + 2,
+        //     (VoxelData.TerrainSize * VoxelData.ChunkDepth) / 2);
+        // player.transform.position = _spawnPosition;
         
         GenerateWorld();
     }
     
-    private void Update()
-    {
-        GenerateChunkAroundPlayer();
-    }
+    // private void Update()
+    // {
+    //     GenerateChunkAroundPlayer();
+    // }
 
     public BlockTypeEnum BlockCondition(Vector3 pos)
     {
@@ -37,17 +37,28 @@ public class MinecraftTerrain : MonoBehaviour
             return BlockTypeEnum.Stone;
     }
     
+    // void GenerateWorld()
+    // {
+    //     for (int x = (VoxelData.TerrainSize / 2) - VoxelData.ViewDistance; x < (VoxelData.TerrainSize / 2) + VoxelData.ViewDistance; x++)
+    //     {
+    //         for (int z = (VoxelData.TerrainSize / 2) - VoxelData.ViewDistance; z < (VoxelData.TerrainSize / 2) + VoxelData.ViewDistance; z++)
+    //         {
+    //             CreateNewChunk(x, z);
+    //         }
+    //     }
+    // }
+    
     void GenerateWorld()
     {
-        for (int x = (VoxelData.TerrainSize / 2) - VoxelData.ViewDistance; x < (VoxelData.TerrainSize / 2) + VoxelData.ViewDistance; x++)
+        for (int x = 0; x < VoxelData.TerrainSize; x++)
         {
-            for (int z = (VoxelData.TerrainSize / 2) - VoxelData.ViewDistance; z < (VoxelData.TerrainSize / 2) + VoxelData.ViewDistance; z++)
+            for (int z = 0; z < VoxelData.TerrainSize; z++)
             {
                 CreateNewChunk(x, z);
             }
         }
     }
-    
+
     Coord Vector3ToCoord(Vector3 pos)
     {
         int x = Mathf.FloorToInt(pos.x) / VoxelData.ChunkWidth;
@@ -56,23 +67,23 @@ public class MinecraftTerrain : MonoBehaviour
         return new Coord(x, z);
     }
     
-    void GenerateChunkAroundPlayer()
-    {
-        Coord playerPos = Vector3ToCoord(player.transform.position);
-        for (int x = playerPos.X - VoxelData.ViewDistance; x < playerPos.X + VoxelData.ViewDistance; x++)
-        {
-            for (int z = playerPos.Z - VoxelData.ViewDistance; z < playerPos.Z + VoxelData.ViewDistance; z++)
-            {
-                if (IsChunkInWorld(x, z))
-                {
-                    if (_chunks[x, z] == null)
-                    {
-                        CreateNewChunk(x, z);
-                    }
-                }
-            }
-        }
-    }
+    // void GenerateChunkAroundPlayer()
+    // {
+    //     Coord playerPos = Vector3ToCoord(player.transform.position);
+    //     for (int x = playerPos.X - VoxelData.ViewDistance; x < playerPos.X + VoxelData.ViewDistance; x++)
+    //     {
+    //         for (int z = playerPos.Z - VoxelData.ViewDistance; z < playerPos.Z + VoxelData.ViewDistance; z++)
+    //         {
+    //             if (IsChunkInWorld(x, z))
+    //             {
+    //                 if (_chunks[x, z] == null)
+    //                 {
+    //                     CreateNewChunk(x, z);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
     
     void CreateNewChunk(int x, int z)
     {
