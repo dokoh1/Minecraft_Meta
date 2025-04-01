6ab1e75be4e19c9e09e782005ce73f88043af66b
TaeyeongHan <dianspace53@gmail.com>
2025-04-01 16:47:05 +0900

    skybox rotation 구현 완료

---
+++ Assets/Scripts/startMenu/SkyboxRotation.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+public class SkyboxRotation : MonoBehaviour
+{
+    // private float _degree;
+    //
+    // void Start()
+    // {
+    //     _degree = 0;
+    // }
+    //
+    // void Update()
+    // {
+    //     _degree += Time.deltaTime;
+    //     if (_degree >= 360) 
+    //         _degree = 0;
+    //     
+    //     RenderSettings.skybox.SetFloat("_Rotation", Mathf.Lerp(0, 1, _degree));
+    //     
+    // }
+    public float rotationSpeed = 2.0f;
+    
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+    }
+    
+}
